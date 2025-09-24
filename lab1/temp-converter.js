63e2626f1de8c7e3d6e604ea4d4cfe58a3b79a20
deborahodumusi-glitch <deborahodumusi@gmail.com>
2025-09-25 00:20:47 +0100

    Add files via upload

---
+++ lab1/temp-converter.js
@@ -0,0 +1,14 @@
+
+// The average temperature in Boston, MA during September is around 66°F.
+let fahrenheit = 66;
+
+// The rest of this code uses  fahrenheit value to do the math.
+let celsius = ((fahrenheit - 32) * 5) / 9;
+let kelvin = ((fahrenheit + 459.67) * 5) / 9;
+
+// This line prints the starting temperature.
+console.log("Temperature (fahrenheit): " + fahrenheit);
+
+// These lines print the calculated results.
+console.log("Temperature (celsius): " + celsius);
+console.log("Temperature (kelvin): " + kelvin);
